dda70f22ed79743ae1d7f789503f69c32c596ee7
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-04-28 13:54:18 +0200

    render image

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,16 @@ use worker::*;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    Response::ok("Hello, World!")
+    let svg_data = include_bytes!("../img.svg");
+    let mut opt = usvg::Options::default();
+    let mut fontdb = fontdb::Database::new();
+    let svg = usvg::Tree::from_data(svg_data, &opt, &fontdb).unwrap();
+
+    let pixmap_size = svg.size().to_int_size();
+    let mut pixmap = tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
+    resvg::render(&svg, tiny_skia::Transform::default(), &mut pixmap.as_mut());
+
+    let mut headers = Headers::new();
+    headers.set("content-type", "image/png")?;
+    Ok(Response::from_bytes(pixmap.data().to_vec())?.with_headers(headers))
 }
